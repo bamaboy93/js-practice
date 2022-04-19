370a9c21968442c23910d563b9b82ab5fc3cd36e
Alexander Krupka <bamaboy483@gmail.com>
2022-04-19 21:59:10 +0300

    sum

---
+++ 7kuy.js
@@ -0,0 +1,34 @@
+// Make a program that filters a list of strings and returns a list with only your friends name in it.
+
+// If a name has exactly 4 letters in it, you can be sure that it has to be a friend of yours! Otherwise, you can be sure he's not...
+
+// Ex: Input = ["Ryan", "Kieran", "Jason", "Yous"], Output = ["Ryan", "Yous"]
+
+// friend ["Ryan", "Kieran", "Mark"] `shouldBe` ["Ryan", "Mark"]
+
+function f(friends) {
+  //   let fellas = [];
+  //   for (friend of friends) {
+  //     if (friend.length === 4) {
+  //       fellas.push(friend);
+  //     }
+  //   }
+  //   return fellas;
+  //
+  //     const list = friends.filter((n) => n.length === 4);
+  // //   return list;
+}
+
+// console.log(f(["Ryan", "Kieran", "Mark"]));
+// /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Find shortest word in the string
+//
+// function findShort(s) {
+//   const words = s.split(" ");
+
+//   const numbers = words.map((word) => word.length);
+
+//   return Math.min(...numbers);
+// }
+
+// console.log(findShort("bitcoin take over the world maybe who knows perhaps"));
--- 8kuy.js
+++ 8kuy.js
@@ -122,3 +122,26 @@
 // console.log(
 //   points(["0:1", "0:2", "0:3", "0:4", "1:2", "1:3", "1:4", "2:3", "2:4", "3:4"])
 // ); //0
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Given an array of integers.
+
+// Return an array, where the first element is the count of positives numbers and the second element is sum of negative numbers. 0 is neither positive nor negative.
+
+// If the input is an empty array or is null, return an empty array.
+
+// Example
+// For input [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15], you should return [10, -65].
+
+// function countPositivesSumNegatives(input) {
+// //   let pos = 0;
+// //   let neg = 0;
+
+// //   input.forEach((num) => (num > 0 ? pos++ : (neg += num)));
+// //   return [pos, neg];
+// }
+
+// console.log(
+//   countPositivesSumNegatives([
+//     1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15,
+//   ])
+// );
